ed0372457bbde515d920597afdad6424164c2af1
Igor <Iiiliev.igor@gmail.com>
2023-02-17 15:05:27 +0200

    das

--- sql_conspect/pspg2.py
+++ sql_conspect/pspg2.py
@@ -4,16 +4,48 @@ from config import db_name, host,port,user,password
 try:
     connection = psycopg2.connect(
         host=host,
+        port=port,
         database=db_name,
         user=user,
         password=password)
 
-    cursor = connection.cursor()
+    connection.autocommit = True
+
+    # with connection.cursor() as cursor:
+    #     cursor.execute(
+    #         """
+    #         CREATE TABLE users(
+    #         id serial PRIMARY KEY,
+    #         name varchar(15) NOT NULL,
+    #         nick_name varchar(25) NOT NULL
+    #         );
+    #         """)
+    #     print(f'Таблица users создана!')
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+            INSERT INTO users (name, nick_name) VALUES ('igor', 'harry94');
+            """
+        )
+        print('Данные добавлены')
+
+    with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT * FROM users;
+                """
+            )
+            for i in cursor.fetchall():
+                print(i)
+
+
 
 
 
 except Exception as ex:
     print(ex)
 finally:
-    pass
+    if connection:
+        connection.close()
 
