007bb157657e413965109b09709c90be6ffae2f4
Igor <Iiiliev.igor@gmail.com>
2023-02-22 14:22:27 +0200

    das

---
+++ conspect_aiogram/10_Mediafiles.py
@@ -0,0 +1,95 @@
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message, ContentType, BotCommand, BotCommandScopeDefault, FSInputFile
+from aiogram.filters import Filter, Command, Text
+import asyncio
+from environs import Env
+import logging #импортируем библиотеку логирования
+
+#Блок инициализации#############################
+env = Env()                                    #
+env.read_env('.env')                           #
+TOKEN = env.str('TOKEN')                       #
+ADMIN = env.int('ADMIN_ID')                    #
+################################################
+#Блок с хэндлерами на отправку медиафайлов
+async def send_audio(message: Message, bot: Bot):
+    audio = FSInputFile(path=f'media_files/audio.m4a')
+
+
+
+
+
+
+
+
+
+
+
+#Блок стартовых функций#########################
+#Установка команд в меню при старте
+async def set_commands(bot: Bot):
+    commands = [
+        BotCommand(command='start', description='👻 Начало'),
+        BotCommand(command='help', description='Описание Бота'),
+        BotCommand(command='inline', description='Карточки товаров'),
+        BotCommand(command='form', description='Заполнить анкету'),
+        BotCommand(command='end_block', description='----------------'),
+
+        #Блок команд на отправку медиа файлов
+            BotCommand(command='send_photo', description='Отправить фото'),
+        BotCommand(command='send_video', description='Отправить видео'),
+        BotCommand(command='send_audio', description='Отправить аудио'),
+        BotCommand(command='send_document', description='Отправить документ'),
+        BotCommand(command='send_mediagroup', description='Отправить несколько медиафайлов'),
+        BotCommand(command='send_sticker', description='Отправить стикер'),
+        BotCommand(command='send_videonote', description='Отправить видеосообщение'),
+        BotCommand(command='send_voice', description='Отправить голосового сообщение'),
+        #######################################
+    ]
+
+    await bot.set_my_commands(commands, BotCommandScopeDefault()) #Скоп по умолчанию|ПОказывает команды всем
+
+async def start_bot(bot: Bot): #функция срабатывает когда запускается сервер с ботом
+    await bot.send_message(ADMIN, text='Бот запущен!')
+    await set_commands(bot)
+
+async def stop_bot(bot: Bot):
+    await bot.send_message(ADMIN, text='<s>Бот остановлен</s>')
+
+async def get_start(message: Message, bot: Bot): #Функция срабатывает когда юзер дает команду /start
+    await message.answer('Давай начнем!')
+
+###############################################
+
+
+#Тело бота#####################################
+async def start():
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - [%(levelname)s] - %(name)s -(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s")
+
+    bot = Bot(token=TOKEN, parse_mode="HTML")
+    dp = Dispatcher()
+    dp.startup.register(start_bot) #Регистрируем хэндлер срабатывающий при запуске
+    dp.shutdown.register(stop_bot)
+
+
+
+    dp.message.register(get_start, Command(commands=['start'])) #Регистрируем хэндлер на команду /start
+
+
+
+
+
+    try:
+        #Начало сессии
+        await dp.start_polling(bot)
+    finally:
+        #Закрываем сессию
+        await bot.session.close()
+###############################################
+
+
+#Запускаем функцию Бота########################
+if __name__ =="__main__":
+    asyncio.run(start())
+
+
