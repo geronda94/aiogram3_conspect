f6e28489f60fb83e6afa20a7e620a75727e3b261
Igor Ubuntu <iiiliev.igor@gmail.com>
2023-02-22 23:22:13 +0200

    Второй комит, добавил ядро бота

---
+++ booking_bot.py
@@ -0,0 +1,73 @@
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message, ContentType
+from aiogram.filters import Filter, Command, Text
+import asyncio
+from environs import Env
+import logging #импортируем библиотеку логирования
+from booking_core.settings import TOKEN, ADMIN
+
+
+################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Блок стартовых функций#########################
+async def start_bot(bot: Bot): #функция срабатывает когда запускается сервер с ботом
+    await bot.send_message(ADMIN, text='Бот запущен!')
+
+async def stop_bot(bot: Bot):
+    await bot.send_message(ADMIN, text='<s>Бот остановлен</s>')
+
+async def get_start(message: Message, bot: Bot): #Функция срабатывает когда юзер дает команду /start
+    await message.answer('Давай начнем!')
+###############################################
+
+
+#Тело бота#####################################
+async def start():
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - [%(levelname)s] - %(name)s -(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s")
+
+    bot = Bot(token=TOKEN, parse_mode="HTML")
+    dp = Dispatcher()
+    dp.startup.register(start_bot) #Регистрируем хэндлер срабатывающий при запуске
+    dp.shutdown.register(stop_bot)
+
+
+
+    dp.message.register(get_start, Command(commands=['start'])) #Регистрируем хэндлер на команду /start
+
+
+
+
+
+    try:
+        #Начало сессии
+        await dp.start_polling(bot)
+    finally:
+        #Закрываем сессию
+        await bot.session.close()
+###############################################
+
+
+#Запускаем функцию Бота########################
+if __name__ =="__main__":
+    asyncio.run(start())
+
+
--- booking_core/settings.py
+++ booking_core/settings.py
@@ -1,33 +1,52 @@
 from environs import Env #моудль для работы с локальными переменными
 from dataclasses import dataclass #модуль с оберткой классов для быстрой инициализации
 
-
 @dataclass
 class Bots:
-    TOKEN : str
-    ADMIN : int
+    bot_token: str
+    admin_id: str
+
+@dataclass
+class DB:
+    user: str
+    host: str
+    port: str
+    password: str
+    db: str
 
 @dataclass
 class Settings:
     bots: Bots
+    db: DB
 
-def get_settings(path: str): #Задаем функцию по выгрузке переменных
+
+def get_settings(path: str):
     env = Env()
-    env.read_env(path)
+    env.read_env(path=path)
+
     return Settings(
-        bots = Bots(TOKEN=env.str('TOKEN'),
-                    ADMIN=env.int('ADMIN_ID'))
-    )
+    bots=Bots(
+        bot_token=env.str('TOKEN'),
+        admin_id=env.str('ADMIN_ID')
+    ),
+    db=DB(
+        user=env.str('DB_USER'),
+        host=env.str('DB_HOST'),
+        port=env.str('DB_PORT'),
+        password=env.str('DB_PASSWORD'),
+        db=env.str('DB_DATABASE')
+    ))
 
-settings = get_settings('.env')  #Создаем набор переменных из окружения .env
-
-TOKEN = settings.bots.TOKEN
-ADMIN = settings.bots.ADMIN
+settings = get_settings('.env')
 
 
-
-# Делает аналогичное коду выше
+#Делает аналогичное коду выше
 # env = Env()
 # env.read_env('.env')
 # TOKEN = env.str('TOKEN')
 # ADMIN = env.int('ADMIN_ID')
+# DB_USER = env.str('DB_USER ')
+# DB_PASSWORD = env.str('DB_PASSWORD')
+# DB_HOST = env.str('DB_HOST')
+# DB_PORT = env.str('DB_PORT')
+# DB_DATABASE = env.str('DB_DATABASE')
